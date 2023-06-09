cb5f24947ad343e35c4e25d9a6284f2a7c33cd31
dahcodingkid <dahcodingkid@gmail.com>
2023-06-09 16:35:36 -0700

    pwa & security fixes

--- src/components/HowItWorks.jsx
+++ src/components/HowItWorks.jsx
@@ -1,9 +1,9 @@
-import img1 from "../assets/1.jpg"
-import img2 from "../assets/2.jpg"
-import img3 from "../assets/3.jpg"
-import img4 from "../assets/4.jpg"
-import img5 from "../assets/5.jpg"
-import img6 from "../assets/6.jpg"
+import img1 from "../assets/1.webp"
+import img2 from "../assets/2.webp"
+import img3 from "../assets/3.webp"
+import img4 from "../assets/4.webp"
+import img5 from "../assets/5.webp"
+import img6 from "../assets/6.webp"
 
 const HowItWorks = () => {
 
